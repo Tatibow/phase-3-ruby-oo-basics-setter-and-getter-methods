c70755495f1b1436d2d8ca2443933a71f59c812b
Tatiana <Bowmantatiana2@gmail.com>
2023-01-31 05:33:29 -0800

    commit tree 88589c7d8475bf06a783049f77154c3cbb1976d9

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,25 @@
 class Dog
+    def name=(name)
+        @name = name
+    end
 
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
 end
+
+peach = Dog.new
+peach.name = "Peach"
+peach.breed = "Pom"
+
+#getters
+peach.name
+peach.breed
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,25 @@
 class Person
+    def name=(name)
+        @name=name
+    end
 
+    def name
+        @name
+    end
+
+    def job=(job)
+        @job=job
+    end
+
+    def job
+        @job
+    end
 end
+
+person = Person.new
+person.name = "Tatiana"
+person.job ="SWE"
+
+#getters
+person.name
+person.job
